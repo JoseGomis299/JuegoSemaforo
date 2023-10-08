a2cf7b1c76306885a13d1928fd7b4632892ab652
Mestre <pablomestre22@gmail.com>
2023-10-08 22:30:05 +0200

    JOSE CALVO
    Añadidos sprites actualizados con borde negro. Hecho que las animaciones se paren al pararse las personas.

--- Semaforo/Assets/Scripts/People/Move.cs
+++ Semaforo/Assets/Scripts/People/Move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,18 +16,30 @@ public class Move : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] Characters character;
+    [SerializeField] private float maxYPos = 6f;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.Space))
+        {
+            anim.speed = 0f;
             return;
+        }
 
+        anim.speed = 1f;
         transform.position += new Vector3(0, speed * Time.deltaTime, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Car")
+        if (collision.CompareTag("Car"))
         {
             if (character != Characters.Tank)
             {
@@ -37,6 +50,14 @@ public class Move : MonoBehaviour
                 collision.gameObject.SetActive(false);
                 Die();
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if (transform.position.y > maxYPos)
+        {
+            gameObject.SetActive(false);
         }
     }
 
