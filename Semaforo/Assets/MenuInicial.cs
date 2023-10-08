c492ab8b02254db4f29434e4099918c9f188d202
LAPTOP-H6AR8AMI\ivanr <ivanrv2005@gmail.com>
2023-10-08 20:30:58 +0200

    estoy terminando el menú

--- Semaforo/Assets/MenuInicial.cs
+++ Semaforo/Assets/MenuInicial.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MenuInicial : MonoBehaviour
 {
+    public GameObject menuprincipal;
     public void Jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        menuprincipal.SetActive(false);
     }
-
     public void Salir()
     {
         Debug.Log("Salir...");
